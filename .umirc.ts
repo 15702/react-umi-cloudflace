635b14986e53bc238ace53fcdac130e663644da3
15702 <1570228878@qq.com>
2025-04-10 15:29:53 +0800

    history

--- .umirc.ts
+++ .umirc.ts
@@ -148,7 +148,7 @@ export default defineConfig({
   codeSplitting:{
     jsStrategy: 'granularChunks'
   },
-  history: { type: 'hash' },
+  // history: { type: 'hash' },
   links: [
     { href: 'https://yala.org', rel: 'canonical' }
   ],
